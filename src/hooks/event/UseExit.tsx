d7cbc31b86c7e1921d86791f4735fd33e832accf
Lorris <51312848+Yourownway@users.noreply.github.com>
2023-03-02 19:33:46 +0100

    refacto(modal): exitEvent hook added

---
+++ src/hooks/event/UseExit.tsx
@@ -0,0 +1,37 @@
+import React, { useCallback, useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { gameState as gameSlice, handleIsPlaying } from "../../store/slice/gameSlice";
+import { EAction } from "../../components/ui/modals/type";
+import { uiState as uiSlice, handleOpenModal } from "../../store/slice/uiSlice";
+
+
+export default function UseExit() {
+    const dispatch = useDispatch();
+    const gameState = useSelector(gameSlice);
+    const uiState = useSelector(uiSlice);
+
+    const onExit = useCallback(
+        (e: KeyboardEvent, action: EAction) => {
+            if (e.key === "Escape") {
+               const isActive = !uiState.modalState.isActive
+                dispatch(handleIsPlaying({ bool: isActive }));
+                   
+                dispatch(
+                    handleOpenModal({ isActive, modalAction: action })
+                );
+           }
+        },
+      [],
+    )
+    
+
+
+    useEffect(() => {
+        console.log("render");
+        
+        window.addEventListener("keydown", (e) => onExit(e, EAction.EXIT));
+        return () => {
+            window.removeEventListener("keydown", (e) => onExit(e, EAction.EXIT));
+        };
+    }, [onExit]);
+}
