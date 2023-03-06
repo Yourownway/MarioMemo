9fb98f528fdcfda4158f71a014e4e25d96420970
Lorris <51312848+Yourownway@users.noreply.github.com>
2023-03-06 10:30:41 +0100

    fix(modal): exit event

--- src/hooks/event/UseExit.tsx
+++ src/hooks/event/UseExit.tsx
@@ -3,15 +3,17 @@ import { useDispatch, useSelector } from "react-redux"
 import { gameState as gameSlice, handleIsPlaying } from "../../store/slice/gameSlice";
 import { EAction } from "../../components/ui/modals/type";
 import { uiState as uiSlice, handleOpenModal } from "../../store/slice/uiSlice";
+import {userState as userSlice} from '../../store/slice/userSlice';
 
 
 export default function UseExit() {
     const dispatch = useDispatch();
     const gameState = useSelector(gameSlice);
     const uiState = useSelector(uiSlice);
-
-    const onExit = useCallback(
+    const userState = useSelector(userSlice);
+    const onExit = 
         (e: KeyboardEvent, action: EAction) => {
+            if(userState.step !== "game") return
             if (e.key === "Escape") {
                const isActive = uiState.modalState.isActive
                 dispatch(handleIsPlaying({ bool: isActive }));    
@@ -19,14 +21,13 @@ export default function UseExit() {
                     handleOpenModal({ isActive: !isActive, modalAction: action })
                 );
            }
-        },
-      [],
-    )
+        }
+    
+    
     
 
 
     useEffect(() => {
-        
         window.addEventListener("keydown", (e) => onExit(e, EAction.EXIT));
         return () => {
             window.removeEventListener("keydown", (e) => onExit(e, EAction.EXIT));
