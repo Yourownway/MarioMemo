f936bd8316e882443a5af94e0f1f7dddde976e97
Lorris <51312848+Yourownway@users.noreply.github.com>
2023-03-06 11:50:59 +0100

    fix(modal): add keydown event to prevent infinite loop

--- src/hooks/event/UseExit.tsx
+++ src/hooks/event/UseExit.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from "react-redux"
 import { gameState as gameSlice, handleIsPlaying } from "../../store/slice/gameSlice";
 import { EAction } from "../../components/ui/modals/type";
 import { uiState as uiSlice, handleOpenModal } from "../../store/slice/uiSlice";
-import {userState as userSlice} from '../../store/slice/userSlice';
+import { userState as userSlice } from '../../store/slice/userSlice';
 
 
 export default function UseExit() {
@@ -11,26 +11,38 @@ export default function UseExit() {
     const gameState = useSelector(gameSlice);
     const uiState = useSelector(uiSlice);
     const userState = useSelector(userSlice);
-    const onExit = 
+
+    const [keyIsDown, setKeyIsDown] = useState(false)
+    const handleKeyDown =
         (e: KeyboardEvent, action: EAction) => {
-            if(userState.step !== "game") return
+            if (keyIsDown) return
+            setKeyIsDown(true)
+            console.log("key press");
+            e.preventDefault()
+            if (userState.step !== "game" || uiState.countDownState.isActive) return
             if (e.key === "Escape") {
-               const isActive = uiState.modalState.isActive
-                dispatch(handleIsPlaying({ bool: isActive }));    
+                const isActive = uiState.modalState.isActive
+                dispatch(handleIsPlaying({ bool: isActive }));
                 dispatch(
                     handleOpenModal({ isActive: !isActive, modalAction: action })
                 );
-           }
+            }
         }
-    
-    
-    
-
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if (e) setKeyIsDown(false)
+    }
 
     useEffect(() => {
-        window.addEventListener("keydown", (e) => onExit(e, EAction.EXIT));
+        window.addEventListener("keydown", (e) => handleKeyDown(e, EAction.EXIT));
         return () => {
-            window.removeEventListener("keydown", (e) => onExit(e, EAction.EXIT));
+            window.removeEventListener("keydown", (e) => handleKeyDown(e, EAction.EXIT));
         };
-    }, [onExit]);
+    }, [handleKeyDown]);
+
+    useEffect(() => {
+        window.addEventListener("keyup", (e) => handleKeyUp(e));
+        return () => {
+            window.removeEventListener("keyup", (e) => handleKeyUp(e));
+        };
+    }, [handleKeyUp]);
 }
